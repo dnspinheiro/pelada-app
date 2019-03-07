f3b83cf21e037b25b572e21502d505c42d95f67f
Dênis Robson Santos Pinheiro <denis.pinheiro@agisrn.com>
2019-03-07 17:35:11 -0300

    Post Pelada

--- feature/src/main/java/com/pelada/panelinha/feature/PeladasActivity.java
+++ feature/src/main/java/com/pelada/panelinha/feature/PeladasActivity.java
@@ -1,5 +1,6 @@
 package com.pelada.panelinha.feature;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -51,6 +52,8 @@ public class PeladasActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
+//                Intent intent = new Intent(this, NovaPelada.class);
+////                startActivity(intent);
             }
         });
     }
